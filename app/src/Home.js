dfa6fd4edccf5a1da019570623d444ee3fc299e8
keerthika <keerthika.r2122@gmail.com>
2024-10-18 06:24:55 +0530

    Removed all files

--- app/src/Home.js
+++
@@ -1,6 +0,0 @@
-function Home(){
-    return(
-        <h1>this is home page</h1>
-    )
-};
-export default Home;
--- myapp/src/Home.js
+++
@@ -1,5 +0,0 @@
-export default function Home(){
-    return(
-        <h1>This is the home component </h1>
-    )
-}
